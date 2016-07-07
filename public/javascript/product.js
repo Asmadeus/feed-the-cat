a7cbd3298806ec162904bdad7f82770571cf1527
Kirill Grevtsov <kgrevtsov@mail.ru>
2016-07-07 16:01:23 +0200

    full task without corner cut

---
+++ public/javascript/product.js
@@ -0,0 +1,23 @@
+$(window).ready(function () {
+  $('.product-item, .product-text__link').click(function () {
+    var parent = $(this).closest('.product-column');
+    if(!parent.hasClass('product-column_disabled')) {
+      parent.toggleClass('product-column_selected');
+      parent.addClass('mouse-in')
+      parent.find('.product-item__text').removeClass('product-item__text_hover').text('Сказочное заморское явство')
+      parent.one('mouseleave', function () {
+        parent.removeClass('mouse-in');
+      })
+    }
+  });
+  $('.product-column').hover(function () {
+    if($(this).hasClass('product-column_selected')) {
+      $(this).find('.product-item__text').text('Котэ не одобряет?').addClass('product-item__text_hover');
+    }
+  },
+  function () {
+    if($(this).hasClass('product-column_selected')) {
+      $(this).find('.product-item__text').text('Сказочное заморское явство').removeClass('product-item__text_hover');
+    }
+  });
+});
